f0bb1e824adaf9eafa314268d2885db28695a44c
Alex Beals <alexcbeals@gmail.com>
2017-09-19 17:59:44 -0400

    Initial Commit

---
+++ screensaver/screensaverView.h
@@ -0,0 +1,13 @@
+//
+//  screensaverView.h
+//  screensaver
+//
+//  Created by Alex Beals on 9/19/17.
+//  Copyright © 2017 Alex Beals. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface screensaverView : ScreenSaverView
+
+@end
