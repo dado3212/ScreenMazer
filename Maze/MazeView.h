6f9517aa94b712861bc916ff1dded6b51705e165
Alex Beals <alexcbeals@gmail.com>
2018-08-12 00:47:41 -0700

    ANOTHER ATTEMPT

---
+++ Maze/MazeView.h
@@ -0,0 +1,13 @@
+//
+//  MazeView.h
+//  Maze
+//
+//  Created by Alex Beals on 8/12/18.
+//  Copyright © 2018 Alex Beals. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MazeView : ScreenSaverView
+
+@end
